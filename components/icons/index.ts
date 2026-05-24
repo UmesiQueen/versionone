3a29669867312a13946a2200e8899a07bf507448
Queen Umesi <queenumesi01@gmail.com>
2026-05-24 10:02:22 +0100

    chore(icons): add brand icons components

---
+++ components/icons/index.ts
@@ -0,0 +1,7 @@
+import Facebook from "./facebook";
+import Instagram from "./instagram";
+import WhatsApp from "./whatsapp";
+import X from "./x";
+import YouTube from "./youtube";
+
+export { Facebook, Instagram, WhatsApp, X, YouTube };
